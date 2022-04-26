e56cb7acb6bafdf5c0c7001e99524f0f88cfa120
Vikas Gavandi <30316401+DIGILABTEAM@users.noreply.github.com>
2022-04-26 11:01:27 +0530

    trial & error

---
+++ src/views/GameHeaderView.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Game.css';
+import PropTypes from 'prop-types';
+// import images from 'images/logo.png';
+
+const GameHeaderView = (props) =>
+    <>
+        <header className='game-header'>
+            <div className='game-title'> CETANIL-TM</div>
+        </header>
+        <div><h2>{props.gameName}</h2></div>
+    </>;
+
+// function Home() {
+//     return (
+//         <div>
+//             <img src={images / logo.png} alt="Centanil.logo" />
+//         </div>
+//     );
+// }
+
+GameHeaderView.propTypes = {
+    gameName: PropTypes.string,
+};
+
+export default GameHeaderView;
