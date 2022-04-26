d90a1e6b8f3a2f26a1b47d2d6cd5e149eddd57af
DIGILABTEAM <30316401+DIGILABTEAM@users.noreply.github.com>
2022-04-26 06:01:22 +0000

    Deploying to gh-pages from @ DIGILABTEAM/veldrop@b8e20072caa0038c640402aeb5ccb4491fbb41fd 🚀

---
+++ precache-manifest.70326b2a38de0e0b196599f686c4c9cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ea6c0d64510d93a90d3",
+    "url": "/veldrop/static/css/main.ede74d55.chunk.css"
+  },
+  {
+    "revision": "5ea6c0d64510d93a90d3",
+    "url": "/veldrop/static/js/main.54b76427.chunk.js"
+  },
+  {
+    "revision": "de5a551dc20d932ecb7f",
+    "url": "/veldrop/static/js/runtime~main.a66264bb.js"
+  },
+  {
+    "revision": "c232e15a41b7d2634e9e",
+    "url": "/veldrop/static/js/2.c1f4db70.chunk.js"
+  },
+  {
+    "revision": "c745ae7e8041b43a08f0",
+    "url": "/veldrop/static/js/3.c76dd483.chunk.js"
+  },
+  {
+    "revision": "9fa628046ff6ab4d992fe9ec805a0327",
+    "url": "/veldrop/index.html"
+  }
+];
